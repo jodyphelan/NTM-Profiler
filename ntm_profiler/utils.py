9a11d15df195d69d274aa4654e7e8c09fabfcd9e
Jody Phelan <jody.phelan@lshtm.ac.uk>
2024-07-25 17:19:15 +0100

    remove old code

--- ntm_profiler/utils.py
+++ ntm_profiler/utils.py
@@ -26,18 +26,18 @@ def summarise_sourmash_hits(sourmash_hits):
             species.append(hit["species"])
     return ";".join(species)
 
-def consolidate_species_predictions(kmer_prediction, sourmash_prediction):
-    filtered_sourmash_prediction = [d for d in sourmash_prediction if d["ani"]>95]
-    if len(kmer_prediction)>1:
-        return None
-    elif len(kmer_prediction)>0 and len(filtered_sourmash_prediction)>0:
-        if kmer_prediction[0]["species"]==filtered_sourmash_prediction[0]["species"]:
-            return kmer_prediction[0]["species"]
-        else:
-            return None
-    elif len(filtered_sourmash_prediction)>0:
-        return filtered_sourmash_prediction[0]["species"]
-    elif len(kmer_prediction)>0:
-        return kmer_prediction[0]["species"]
-    else:
-        return None
+# def consolidate_species_predictions(kmer_prediction, sourmash_prediction):
+#     filtered_sourmash_prediction = [d for d in sourmash_prediction if d["ani"]>95]
+#     if len(kmer_prediction)>1:
+#         return None
+#     elif len(kmer_prediction)>0 and len(filtered_sourmash_prediction)>0:
+#         if kmer_prediction[0]["species"]==filtered_sourmash_prediction[0]["species"]:
+#             return kmer_prediction[0]["species"]
+#         else:
+#             return None
+#     elif len(filtered_sourmash_prediction)>0:
+#         return filtered_sourmash_prediction[0]["species"]
+#     elif len(kmer_prediction)>0:
+#         return kmer_prediction[0]["species"]
+#     else:
+#         return None
