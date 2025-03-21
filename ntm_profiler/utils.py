53cd658ed0d6d19f6b14c33ddf43cfa867608ce1
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-03-21 17:30:35 +0100

    merge sourmash species predictions

--- ntm_profiler/utils.py
+++ ntm_profiler/utils.py
@@ -2,6 +2,7 @@ import sys
 import pathogenprofiler as pp
 import argparse
 from pathogenprofiler.models import SpeciesPrediction
+from typing import List
 
 def check_for_databases(args: argparse.Namespace):
     if len(pp.list_db(args.db_dir))<1:
@@ -23,3 +24,17 @@ def summarise_sourmash_hits(sourmash_hits):
         if hit["species"] not in species:
             species.append(hit["species"])
     return ";".join(species)
+
+
+def merge_sourmash_species(sourmash_hits: SpeciesPrediction) -> None:
+    species_detected = set(s.species for s in sourmash_hits.species)
+    species_objects = []
+    for species in species_detected:
+        hits = sorted([s for s in sourmash_hits.species if s.species == species],key=lambda x: x.prediction_info['abundance'],reverse=True)
+        species_objects.append(hits[0])
+
+    total_abundance = sum([s.prediction_info['abundance'] for s in species_objects])
+    for s in species_objects:
+        s.prediction_info['relative_abundance'] = s.prediction_info['abundance']/total_abundance*100
+    sourmash_hits.species = species_objects
+
